8770c1053798a2d4d659413b5b0fa36636c9d484
DinethDamsaraB <dinethdamsarab@gmail.com>
2024-02-01 12:10:43 +0530

    last commot

--- HPlusSportAPI/Controllers/ProductsController.cs
+++ HPlusSportAPI/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using HPlusSportAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HPlusSportAPI.Controllers
 {
@@ -16,10 +17,98 @@ namespace HPlusSportAPI.Controllers
         }
 
         [HttpGet]
-        public ActionResult GetAllProducts()
+        public async Task<ActionResult> GetAllProducts()
         {
-           return Ok(_context.Products.ToArray());
+           return Ok(await _context.Products.ToArrayAsync());
 
         }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync( id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+           /* if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+           */
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                "GetProduct",
+                new { id = product.Id },
+                product);
+        }
+        [HttpPut("id")]
+        public async Task<ActionResult> PutProducts(int id, Product product)
+        {
+            if (id ! = product.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(product).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+            if (!_context.Products.Any(p => p.Id == id){
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await _content.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        [HttpPost]
+        [Route("Delete")]
+
+        public async Task<ActionResult> DeleteMultiple([FromQuery]int[] ids)
+        {
+            var products = new List<Product>();
+            foreach var (var id in ids)
+            {
+                var product = await _context.Products.ForEachAsync(id);
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                products.Add(product);
+
+            }
+
+            _context.Products.RemoveRange(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(products);
+        }
+
     }
 }
